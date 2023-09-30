4c2201f793d30fc47607ccc55fe317d759b13ed1
Thnderbread <david.kiiing1227@gmail.com>
2023-09-30 17:51:08 -0400

    Adjusted base URL for production testing.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = 'https://library-project-backend.vercel.app/';
+// const BASE_URL = 'https://library-project-backend.vercel.app/';
+const BASE_URL = 'localhost:8080';
 
 export default axios.create({
     baseURL: BASE_URL,
